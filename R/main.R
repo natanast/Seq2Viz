9b2b63abda68e6f70baa02207262c4176830a65c
NatAnastas <anastasiadounat@gmail.com>
2025-08-06 12:44:44 +0300

    Volcano tab

--- R/main.R
+++ R/main.R
@@ -18,8 +18,19 @@ ui <- navbarPage(
                      tableOutput("deseq_preview")
                  )
              )
+    ),
+    
+    tabPanel("Volcano Plot",
+             sidebarLayout(
+                 sidebarPanel(
+                     numericInput("logfc_cutoff", "Log2FC cutoff", 1),
+                     numericInput("padj_cutoff", "Adjusted p-value cutoff", 0.05)
+                 ),
+                 mainPanel(
+                     plotOutput("volcano_plot", height = "600px")
+                 )
+             )
     )
-
 )
 
 server <- function(input, output, session) {
@@ -43,7 +54,23 @@ server <- function(input, output, session) {
         head(deseq_data(), 10)
     })
     
-
+    output$volcano_plot <- renderPlot({
+        req(deseq_data())
+        df <- deseq_data()
+        
+        req(all(c("log2FoldChange", "padj") %in% colnames(df)))
+        
+        df$Significant <- with(df,
+                               ifelse(padj < input$padj_cutoff & abs(log2FoldChange) > input$logfc_cutoff,
+                                      "Significant", "Not Significant"))
+        
+        ggplot(df, aes(x = log2FoldChange, y = -log10(padj), color = Significant)) +
+            geom_point(alpha = 0.6) +
+            scale_color_manual(values = c("gray", "red")) +
+            theme_minimal(base_size = 14) +
+            labs(title = "Volcano Plot", x = "log2(Fold Change)", y = "-log10(Adjusted p-value)") +
+            theme(legend.position = "top")
+    })
 }
 
 shinyApp(ui, server)
